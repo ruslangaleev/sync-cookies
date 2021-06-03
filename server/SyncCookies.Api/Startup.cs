56416690e5a811dbecac9d2efb0a78fbcde246d3
Ruslan Galeev <ruslan.nafisovich@gmail.com>
2021-06-03 17:59:23 +0500

    Добавлен ci настройки для prod'a

--- server/SyncCookies.Api/Startup.cs
+++ server/SyncCookies.Api/Startup.cs
@@ -94,10 +94,10 @@ namespace SyncCookies.Api
                 //This is to generate the Default UI of Swagger Documentation  
                 swagger.SwaggerDoc("v1", new OpenApiInfo  
                 {   
-                    Version= "v1",   
-                    Title = "JWT Token Authentication API",  
-                    Description="ASP.NET Core 3.1 Web API" });  
-                // To Enable authorization using Swagger (JWT)  
+                    Version= "Prev Commit fbfea09667fb259cccbddf8df4bf73a8d0c5fdaa",
+                    Title = "JWT Token Authentication API",
+                    Description="ASP.NET Core 3.1 Web API" });
+                // To Enable authorization using Swagger (JWT)
                 swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
                 {  
                     Name = "Authorization",  
@@ -107,12 +107,12 @@ namespace SyncCookies.Api
                     In = ParameterLocation.Header,  
                     Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",  
                 });  
-                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement  
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
                 {  
                     {  
-                          new OpenApiSecurityScheme  
+                          new OpenApiSecurityScheme
                             {  
-                                Reference = new OpenApiReference  
+                                Reference = new OpenApiReference
                                 {  
                                     Type = ReferenceType.SecurityScheme,  
                                     Id = "Bearer"  
