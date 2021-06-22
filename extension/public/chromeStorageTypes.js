18323c970a0ffc9dcb5c6f9d74f04998ab3f6a8f
Ruslan Galeev <ruslan.nafisovich@gmail.com>
2021-06-22 14:52:13 +0500

    Отредактирована строка подключения

--- extension/public/chromeStorageTypes.js
+++ extension/public/chromeStorageTypes.js
@@ -3,5 +3,5 @@ const COOKIE_INFOES = 'COOKIE_INFOES';
 const IS_ENABLE = 'IS_ENABLE';
 const ACCESS_TOKEN = 'ACCESS_TOKEN';
 const UPDATE_FROM_SERVER = 'UPDATE_FROM_SERVER';
-//const SERVER_ADDRESS = 'http://cookiestorage.ru';
-const SERVER_ADDRESS = 'http://localhost:58674';
+const SERVER_ADDRESS = 'http://cookiestorage.ru';
+//const SERVER_ADDRESS = 'http://localhost:58674';
